2d2c8ff415f6901335b02d47d98e5a32dcc33858
Claus Polanka <claus.polanka@chello.at>
2022-12-11 15:34:06 +0100

    Add comment with example

--- src/sageniuz/Main.java
+++ src/sageniuz/Main.java
@@ -2,6 +2,7 @@ package sageniuz;
 
 public class Main {
     public static void main(String[] args) {
-
+        int wordCount = new WordCounter().countWords("hello");
     }
+
 }
---
+++ src/sageniuz/WordCounter.java
@@ -0,0 +1,30 @@
+package sageniuz;
+
+public class WordCounter {
+
+    /**
+     * Input text's lines are split by white space(s). Derived word candidates
+     * will only be counted if word candidates consist of latin alphabet
+     * characters only.
+     * <p/>
+     * Examples:
+     * <pre>
+     *                "inputText" / word count
+     *                "hello" / 1
+     *          "hello world" / 2
+     *  "  hello    world   " / 2
+     *               "hello," / 0
+     *               "hello." / 0
+     *               "hel1lo" / 0
+     *              "$hel1lo" / 0
+     *         "hello, world" / 1
+     *         "hello\nworld" / 2
+     * </pre>
+     *
+     * @param inputText text to count words from
+     * @return word count
+     */
+    public int countWords(String inputText) {
+        return 0;
+    }
+}
